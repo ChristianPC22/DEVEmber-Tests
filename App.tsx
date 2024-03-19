f2de20a2da60b85d78ae034e19628cdfbcd0718d
ChristianPC22 <christianperaza22@gmail.com>
2024-03-19 13:13:14 +0000

    Added styles to the main title text and created a resources folder

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style = {styles.container}>
+      <Text style = {styles.text}>Cute Border Collie with black and white hair</Text>
+
+      <View style = {styles.imageContainer}>
+        <Image source = {require('./resources/img/borderCollie.jpg')} style = {styles.image}></Image>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blanchedalmond',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    fontSize: 24,
+    textAlign: 'center',
+    width: 250,
+    marginBottom: 10,
+  },
+
+  image: {
+    width: 300,
+    height: 300
+  },
+
+  imageContainer: {
+    borderRadius: 10,
+    overflow: 'hidden'
+  }
 });
