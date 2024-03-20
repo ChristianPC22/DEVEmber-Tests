583403384c06e23c02df1095392985087a2a877c
ChristianPC22 <christianperaza22@gmail.com>
2024-03-20 13:35:36 +0000

    Added an SplashScreen if the fonts aren't loaded or they give an error

--- App.tsx
+++ App.tsx
@@ -3,21 +3,31 @@ import { StyleSheet, Text, View, FlatList, ActivityIndicator} from 'react-native
 import DayListItem from './src/components/core/DayListItem';
 
 import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+// 
+SplashScreen.preventAutoHideAsync();
 
 // We have an array with rest parameters (24), then iterate through every element and adding 1 to the index 
 const days = [...Array(24)].map((val, index) => index + 1);
 
 // Parent component
 export default function App() {
-
   {/* Inside the array we create the variables fontsLoaded and fontError, using the map 'useFonts' we specify the key and the value of the fonts we want to load */}
   const [fontsLoaded, fontError] = useFonts({
     Inter: Inter_900Black,
   });
 
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError])
+
   {/* If the fonts weren't loaded and there's no errors, we return a simple charge animation until the fonts are charged */}
-  if (!fontsLoaded && !fontError){
-    return <ActivityIndicator/>;
+  if (!fontsLoaded && !fontError) {
+    return null;
   }
 
   return (
