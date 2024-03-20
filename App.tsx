6edf9b1cd0790c6890b21803047ebdf3e147f3b7
ChristianPC22 <christianperaza22@gmail.com>
2024-03-20 12:30:33 +0000

    Added TypeScript types in the object DayListItem and putted in the props of the function DayListItem

--- App.tsx
+++ App.tsx
@@ -1,32 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList} from 'react-native';
+import DayListItem from './src/components/core/DayListItem';
 
 // We have an array with rest parameters (24), then iterate through every element and adding 1 to the index 
 const days = [...Array(24)].map((val, index) => index + 1);
 
+// Parent component
 export default function App() {
   return (
     <View style = {styles.container}>
-
       <FlatList
         data = {days}
         contentContainerStyle = {styles.content}
         columnWrapperStyle = {styles.column}
         numColumns = {2}
-        renderItem = {({ item }) => (
-          <View style = {styles.box}>
-            <Text style = {styles.text}>{item}</Text>
-          </View>
-        )}
-      />
-
-      {/* Iterate every element in the array and return their result, having a unique key that represents every day */}
-      {/*days.map(day => (
-        <View style = {styles.box} key = {day}>
-          <Text style = {styles.text}>{day}</Text>
-        </View>
-      ))*/}
-
+        renderItem = {({ item }) => <DayListItem day = {item}/>}
+        />
       <StatusBar style="auto" />
     </View>
   );
@@ -47,23 +36,4 @@ const styles = StyleSheet.create({
   column: {
     gap: 10    
   },
-
-  box: {
-    backgroundColor: '#F9EDE3',
-    flex: 1,
-    aspectRatio: 1,
-
-    borderWidth: 1,
-    // borderWidth: StyleSheet.hairlineWidth,
-    borderColor: '#9B4521',
-    borderRadius: 20,
-
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  text: {
-    color: '#9b4521',
-    fontSize: 70
-  }
 });
